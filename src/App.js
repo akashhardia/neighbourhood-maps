3d0a861c4e767691723fa8da7f7a0469291afb75
Akash Hardia <aakashhardiya@gmail.com>
2018-12-12 01:30:33 +0530

    loadingmaps

--- src/App.js
+++ src/App.js
@@ -1,28 +1,69 @@
+import axios from 'axios';
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+
+	componentDidMount(){
+		this.getVenuesFromAPI();
+		this.mapRenderer();
+	}
+
+	state = {
+		venues:[]
+	} 
+
+	mapRenderer = () => {
+		scriptLoader("https://maps.googleapis.com/maps/api/js?key=AIzaSyDXdfy8blk2k0sCJhOeAIfGbRRD0BxfkNQ&callback=initMap");
+		window.initMap = this.initMap;
+		
+	}
+
+	getVenuesFromAPI = () => {
+		let fsEndPoint = "https://api.foursquare.com/v2/venues/explore?";
+		var params = {
+			client_id : "QTKZZQBMCOBKZKY0CH1GJKN3VCIKRBFBQHG4KSWYOPIQCZNC",
+			client_secret : "ADEAW5I1J0OAZ3AY0BQ0HHHTH5DUCBI4XGBBEBYZRACLNLOY",
+			query : "food",
+			near : "Sydney",
+			v : "20182507"
+		}
+
+		axios.get(fsEndPoint+ new URLSearchParams(params)).then(res=>{
+			this.setState({
+				venues: res.data.response.groups[0].items 
+			})  
+			console.log(res)
+		}).catch(error=>{
+			console.log("error "+error)
+		})
+	}
+
+	initMap = () => {
+        let map = new window.google.maps.Map(document.getElementById('map'), {
+          center: {lat: -34.397, lng: 150.644},
+          zoom: 8
+        });
+    }
+
+	render() {
+	    return (
+	      <main>
+	      	<div id='map'></div>
+	      </main>
+	    );
+	}
 }
+
+
+function scriptLoader(toLoad){
+	var script0 = window.document.getElementsByTagName('script')[0];
+	var scriptElem = window.document.createElement('script');
+	scriptElem.src = toLoad;
+	scriptElem.async = true;
+	scriptElem.defer = true;
+	script0.parentNode.insertBefore(scriptElem, script0);
+}
+
 
 export default App;
