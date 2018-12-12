9734701976bfec7fb5dbd88443d92301b6920cea
Akash Hardia <aakashhardiya@gmail.com>
2018-12-13 00:05:40 +0530

    filterfunctionality

--- src/VenuesList.js
+++ src/VenuesList.js
@@ -1,23 +1,64 @@
 import React, { Component } from 'react';
+import {PropTypes} from 'prop-types'
+import escapeRegExp from 'escape-string-regexp'
+import sortBy from 'sort-by'
 
 class VenuesList extends Component{
 
+	static propTypes = {
+    	venues: PropTypes.array.isRequired,
+    	filter: PropTypes.func.isRequired
+  	}
 
+  	state={
+  		query: ""
+  	}
+  	 
+  	updateQuery = (query) => {
+	    this.setState({ query: query })
+	}
 
 	render(){
 
+		let props = this.props;
+		const { query } = this.state
+
+
+	    let showingVenues
+	    if (query) {
+	      const match = new RegExp(escapeRegExp(query), 'i')
+	      showingVenues = props.venues.filter((data) => match.test(data.venue.name))
+	    } else {
+	      showingVenues = props.venues
+	    }
+
+	    showingVenues.sort(sortBy('name'))
+
+
 		 return (
-            <div class="options-box">
-		        <h1>Find Your New NYC Home</h1>
+
+            <div className="side-bar">
+		        <h1>Restaurant Locations</h1>
 		        
-		        <div> 
-		          <span class="text">Search for nearby places</span>
-		          <input id="places-search" type="text" placeholder="Ex: Pizza delivery in NYC"/>
-		          <input id="go-places" type="button" value="Go"/>
-		        </div>
+		        
+		        <input
+		            className='filter-input'
+		            type='text'
+		            placeholder='search'
+		            value={query}
+		            onChange={(event) => this.updateQuery(event.target.value)}
+		        />
+		        
+		        <ul className='venue-list'>
+		        	{ 
+		        		showingVenues.map((data,i)=>{
+						if(i<15){ 
+							return <li onClick={ function(j) { return function() {props.setVenue(j)} }(data) } key={i}>{data.venue.name}</li>
+						}
+					})}
+		        </ul>
 		      </div>
         );
-
 	}
 }
 
